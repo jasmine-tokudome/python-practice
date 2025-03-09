0fe18d5098ff0495f99dac64bcf9cd0bf094457c
Jasmine <103996128+jasmine-tokudome@users.noreply.github.com>
2025-03-09 09:46:21 +0900

    コマンドライン引数を受け取れるようにする

--- python_test/encoder.py
+++ python_test/encoder.py
@@ -1,4 +1,12 @@
 import base64
+import sys
 
 def str_to_base64(x):
     return base64.b64encode(x.encode('utf-8'))
+
+print(str_to_base64(sys.argv[1]))
+
+# sys.argvはpython3コマンド以降の値を要素に持つリスト
+# ※つまりターミナル上では「python3 sys.argv[0] sys.argv[0]」という意味。
+
+# print(sys.argv)
