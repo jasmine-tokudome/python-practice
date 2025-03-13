e9d664cd6511ec5127045e14977ddf57c2e68ed3
Jasmine <103996128+jasmine-tokudome@users.noreply.github.com>
2025-03-13 20:21:17 +0900

    関数の呼び出しをシンプルにする

--- b64/__init__.py
+++ b64/__init__.py
@@ -1,1 +1,11 @@
+from .encoder import str_to_base64
+from .decoder import base64_to_str
+
 print(f'init: {__name__}')
+
+# >>> import b64
+# init: b64
+# >>> b64.str_to_base64('phtyon')
+# b'cGh0eW9u'
+# >>> dir(b64)
+# ['__builtins__', '__cached__', '__doc__', '__file__', '__loader__', '__name__', '__package__', '__path__', '__spec__', 'base64_to_str', 'decoder', 'encoder', 'str_to_base64']
