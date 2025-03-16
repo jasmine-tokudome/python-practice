f22f0fa30161427c827d748d43f1a82d8ccb5f86
Jasmine <103996128+jasmine-tokudome@users.noreply.github.com>
2025-03-16 19:39:49 +0900

    allの活用

--- b64/__init__.py
+++ b64/__init__.py
@@ -2,10 +2,3 @@ from .encoder import *
 from .decoder import *
 
 print(f'init: {__name__}')
-
-# >>> import b64
-# init: b64
-# >>> b64.str_to_base64('phtyon')
-# b'cGh0eW9u'
-# >>> dir(b64)
-# ['__builtins__', '__cached__', '__doc__', '__file__', '__loader__', '__name__', '__package__', '__path__', '__spec__', 'base64_to_str', 'decoder', 'encoder', 'str_to_base64']
